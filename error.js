e45df558ab7a583e9145f2a28c0a3d09ba565d99
Subha Mandal <168220649+SubhaMandal@users.noreply.github.com>
2025-05-09 23:32:18 +0530

    Add files via upload

---
+++ error.js
@@ -0,0 +1,23 @@
+function addTwoNums(){
+    var a;
+    var b;
+    console.log(a+b);
+}
+addTwoNums()
+function addTwoNumss(){
+    var a =5;
+    var b ="5";
+    console.log(a+b);
+}
+addTwoNumss()
+function addTwoNumsss(){
+    try{
+        console.log(a+b);
+    }
+    catch(err){
+        console.log(err);
+        console.log("code is wrong");
+    }
+    
+}
+addTwoNumsss()
