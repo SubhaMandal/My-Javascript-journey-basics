5c3e5cd8ce24bb5d7168785a7ddae4ff5874109b
Subha Mandal <168220649+SubhaMandal@users.noreply.github.com>
2025-05-10 15:53:35 +0530

    Add files via upload

---
+++ function with para and argu.js
@@ -0,0 +1,9 @@
+function sum(a,b){ // here function sum have two parameter declared 
+    var a;
+    var b;
+    var c = a+b;
+    console.log(c);
+}
+sum(2,2) /* here we are usuing two argument which is using preveously declared parameter a,b and 
+replaceing with the value 2,2 */
+sum(5,6)
