a6c7d163abd1248fde7e4b49551f2361cec6fff7
Subha Mandal <168220649+SubhaMandal@users.noreply.github.com>
2025-05-11 14:44:03 +0530

    Add files via upload

---
+++ varriable.js
@@ -0,0 +1,12 @@
+var petdog = "Rex"
+var petcat = "pepper"
+console.log(petdog);
+console.log(petcat);
+console.log("My pet dog's name is: ", petdog);
+console.log("My pet cat's name is: ", petcat);
+var catsound = "purr"
+var dogsound = "woof"
+console.log(petdog, "says", dogsound);
+console.log(petcat, "says", catsound);
+catsound = "meow"
+console.log(petcat, "now says", catsound);
